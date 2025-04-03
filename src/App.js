48fae25a3ef48d3836b375a137170d4658b81389
Nyakorare <g1galba042804@gmail.com>
2025-04-03 21:51:54 +0800

    init tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        (NBA) Nyako-Bet Assistant
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
