5dabde8473dce8708e5f7a17b55005bd1f3ffc28
Nyakorare <g1galba042804@gmail.com>
2025-04-07 00:26:38 +0800

    minor big fixes

--- src/pages/dashboard-components/TeamCards.js
+++ src/pages/dashboard-components/TeamCards.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FiSearch, FiFilter } from "react-icons/fi";
 import Button from "../../components/ui/button";
 import { filterOptions, getLogoFilename } from "../Dashboard";
